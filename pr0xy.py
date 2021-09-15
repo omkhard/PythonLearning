bf3c9a40c15c122cced11b24e8c20f72716264bd
omkhard <omkhard09@gmail.com>
2021-09-15 08:03:06 -0400

    First Commit

---
+++ pr0xy.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""here we will import all the other necessaties 
+and try to make a proxy"""
+import socket ,  requests , urllib.request
+import sniff
+x = sniff.main.raw
+print(x)
